3985d5d1ce7741ebc402a75b099ff6f2d6751bc8
Nguyen <nho.96@live.com>
2014-09-12 19:34:55 +1000

    New blank pages

---
+++ tickets.php
@@ -0,0 +1,38 @@
+<html>
+
+  <head>
+    <meta charset="UTF-8">
+    <title>Bloo-Roo Pictures | Home</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+
+  <body>
+
+    <header>
+        <div> <img src="images/logo.jpg" alt="bloo roo logo" width="140" height="70"/> Bloo Roo Pictures </div>
+    </header>
+	
+    <nav>
+		<ul>
+		<li><a href="index.html">Home</a></li> 
+		<li><a href="movies.html">Movies</a></li>
+		<li><a href="tickets.php">Tickets</a></li>
+		<li><a href="contact.html">Contact Us</a></li>
+		</ul>
+    </nav>
+
+    <main>
+	stuff
+	</main>
+
+    <footer>
+      &copy; Bloo Roo Pictures 2014 | Nguyen Ho s3492627 & Lacey Attridge s3491782 | 
+      <a target="_blank" href="http://validator.w3.org/check?uri=referer"><img style="border:0;height:31px"
+        src="http://www.w3.org/html/logo/downloads/HTML5_Logo_32.png" alt="HTML Validator" /></a> |
+      <a target="_blank" href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0; height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Validator"/></a>
+    </footer>
+
+  </body>
+
+</html>
